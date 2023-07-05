944b9b7977a1bad29a54fa1df189001e8a477255
sumin305 <gogotnals@naver.com>
2023-07-05 18:29:43 +0900

    [Feat] 럭키카드 클래스 구현

---
+++ LuckyCardGame/LuckyCardGame/LuckyCard/LuckyCardManager.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+struct CardManager {
+    
+    private var cardsArray: [LuckyCard] = []
+    private let cardNumberRange = 1...12
+    
+    mutating func makeAllCards() {
+        Animal.allCases.forEach { animal in
+            for number in cardNumberRange {
+                self.cardsArray.append(LuckyCard(number: number, animal: animal, state: .back))
+            }
+        }
+    }
+    
+    private func printCards() {
+        let randomCardsArray = cardsArray.shuffled()
+        let randomCardsStringArray = randomCardsArray.map{"\($0.animal)\($0.number < 10 ? "0" + String($0.number) : String($0.number))"}
+        print(randomCardsStringArray.joined(separator: ", "))
+    }
+}
