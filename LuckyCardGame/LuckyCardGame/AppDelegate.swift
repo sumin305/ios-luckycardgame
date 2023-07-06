94549fab4900ff9fffd2457492199bce0df0b6a5
SOOM <gogotnals@naver.com>
2023-07-06 17:16:06 +0900

    [이수민] W1-2 럭키카드 클래스 구현하기 (#44)
    * [Docs] 커밋 메시지 추가
    
    * [Refactor] 파일 분리 및 전역 변수 삭제
    
    * [Feat] 럭키카드 클래스 구현
    
    * W1-2 럭키카드 클래스 구현하기
    
    * W1-2 럭키카드 클래스 구현하기
    
    * [W1-2] 럭키카드 클래스 구현하기
    
    * [Docs] W1-2 README.md 추가
    
    * [Docs] README.md 추가
    
    * [Chore] 주석 작성 및 enum 구조 변경
    
    * [Chore] 접근제어자 지정

--- LuckyCardGame/LuckyCardGame/AppDelegate.swift
+++ LuckyCardGame/LuckyCardGame/AppDelegate.swift
@@ -1,16 +1,7 @@
-//
-//  AppDelegate.swift
-//  LuckyCardGame
-//
-//  Created by 숨 on 2023/07/04.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
